8bab58a5b99322f17e0d31438f71cd95eb3ad6de
NephygTdkingz <tdkingzict@gmail.com>
2025-05-16 16:05:43 +0100

    finished adding contact page

--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'frontend/index.html')
+
+def about(request):
+    return render(request, 'frontend/about.html')
+
+def contact(request):
+    return render(request, 'frontend/contact.html')
