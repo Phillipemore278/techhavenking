225e1e4fc8c33f1d1921b148618ea7f789014e1d
NephygTdkingz <tdkingzict@gmail.com>
2025-05-16 16:32:17 +0100

    added vercel way of deploying

--- techhaven/settings.py
+++ techhaven/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ob%+@b@9!06(u!^ef!ui^^fggn*e%c8du@1w(l7dic1l9t$1rl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
